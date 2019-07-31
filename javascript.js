dfa7601890dc23e2a981578d3b331e374c8c4236
WillPaulRyan <willpaulryan@gmail.com>
2019-07-30 23:51:48 -0500

    Add support for non-capital characters

--- javascript.js
+++ javascript.js
@@ -1,8 +1,6 @@
 function moveText() {
   // Copies everything from first input into second input, w/ changes
-  document.getElementById("output").value = document.getElementById("input").value
-                                              .toUpperCase()
-                                              .replace(/[^A-Z\s]/g, '');
+  document.getElementById("output").value = document.getElementById("input").value;
 }
 
 function randomize(input) {
@@ -13,11 +11,17 @@ function randomize(input) {
   for (let i = 0; i <input.length; i++) {
     c = input.charAt(i);
 
-    if (c.match(/\s/)) {
-      result += c;
+    if (c.match(/[A-Z]/)) {
+      result += String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    }
+    else if (c.match(/[a-z]/)) {
+      result += String.fromCharCode(97 + Math.floor(Math.random() * 26));
+    }
+    else if (c.match(/[0-9]/)) {
+      result += String.fromCharCode(48 + Math.floor(Math.random() * 10));
     }
     else {
-      result += String.fromCharCode(65 + Math.floor(Math.random() * 26));
+      result += c;
     }
   }
   return result;
@@ -30,14 +34,13 @@ async function monkeys() {
 
   while (original !== test) {
     document.getElementById("counter").innerHTML = counter + " monkey attemps...";
-    // await sleep(.1)
 
     if (counter % 5000 == 0) {
       await sleep(.1);
       document.getElementById("output").value = test;
     }
 
-    if (counter >= 500000) {
+    if (counter >= 1000000) {
       console.log("timeout");
       break;
     }
@@ -48,10 +51,10 @@ async function monkeys() {
   document.getElementById("output").value = test;
 
   if (test == original) {
-    document.getElementById("counter").innerHTML = counter + " monkeys were needed to copy your text.";
+    document.getElementById("counter").innerHTML = "It took " + counter + " monkeys to copy your text.";
   }
   else {
-    document.getElementById("counter").innerHTML = "After" + counter + " attempts, your monkeys gave up.";
+    document.getElementById("counter").innerHTML = "After " + counter + " attempts, our monkeys gave up.";
   }
 }
 
