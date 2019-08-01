d8e1f950fba0ba798e1f58841957f5370da07703
WillPaulRyan <willpaulryan@gmail.com>
2019-07-31 19:04:32 -0500

    Add Single-letter mode

--- javascript.js
+++ javascript.js
@@ -3,10 +3,81 @@ function moveText() {
   document.getElementById("output").value = document.getElementById("input").value;
 }
 
+async function monkeys() {
+  output = document.getElementById("output");
+  counter = document.getElementById("counter");
+  const original = output.value;
+  test = randomize(original);
+  let count = 1;
+
+  while (original !== test) {
+    counter.innerHTML = count.toLocaleString() + " monkeys typing away...";
+
+    if (count % 1000 == 0) {
+      await sleep(.01);
+      output.value = test;
+    }
+
+    if (count >= 10000000) {
+      console.log("timeout");
+      break;
+    }
+    test = randomize(original);
+    count++;
+  }
+
+  output.value = test;
+
+  if (test == original) {
+    counter.innerHTML = count.toLocaleString() + " monkeys were needed to copy your text.";
+  }
+  else {
+    counter.innerHTML = "After " + count.toLocaleString() + " attempts, our monkeys gave up.";
+  }
+}
+
+async function monkeySingle() {
+  out = document.getElementById("output");
+  counter = document.getElementById("counter");
+  const original = out.value;
+  out.value = randomize(original);
+  test = out.value.split('');
+  let count = 1;
+
+  console.log(test);
+
+  while (test.join('') != original) {
+    for (let i = 0; i <test.length; i++) {
+      while (test[i] != original.charAt(i)) {
+
+        if (test[i].match(/[A-Z]/)) {
+          test[i] = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+          await sleep(.1);
+          out.value = test.join('');
+        }
+
+        else if (test[i].match(/[a-z]/)) {
+          test[i] = String.fromCharCode(97 + Math.floor(Math.random() * 26));
+          await sleep(.1);
+          out.value = test.join('');
+        }
+
+        else if (test[i].match(/\d/)) {
+          test[i] = String.fromCharCode(48 + Math.floor(Math.random() * 10));
+          await sleep(.1);
+          out.value = test.join('');
+        }
+        counter.innerHTML = count.toLocaleString() + " monkeys typing away...";
+        count++;
+      }
+    }
+  }
+  counter.innerHTML = count.toLocaleString() + " monkeys were needed to copy your text.";
+}
+
 function randomize(input) {
   let result = '';
   let c = '';
-
 
   for (let i = 0; i <input.length; i++) {
     c = input.charAt(i);
@@ -25,37 +96,6 @@ function randomize(input) {
     }
   }
   return result;
-}
-
-async function monkeys() {
-  const original = document.getElementById("output").value;
-  test = randomize(original);
-  let counter = 1;
-
-  while (original !== test) {
-    document.getElementById("counter").innerHTML = counter + " monkey attemps...";
-
-    if (counter % 5000 == 0) {
-      await sleep(.1);
-      document.getElementById("output").value = test;
-    }
-
-    if (counter >= 1000000) {
-      console.log("timeout");
-      break;
-    }
-    test = randomize(original);
-    counter++;
-  }
-
-  document.getElementById("output").value = test;
-
-  if (test == original) {
-    document.getElementById("counter").innerHTML = "It took " + counter + " monkeys to copy your text.";
-  }
-  else {
-    document.getElementById("counter").innerHTML = "After " + counter + " attempts, our monkeys gave up.";
-  }
 }
 
 function sleep(ms) {
